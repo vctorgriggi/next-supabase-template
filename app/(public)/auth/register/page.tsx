012d036d85464c495e986eceee16ef3ea7459bb1
vctorgriggi <victorgriggi21@gmail.com>
2025-12-12 18:19:05 -0400

    Update login and register page UI and form components

--- app/(public)/auth/register/page.tsx
+++ app/(public)/auth/register/page.tsx
@@ -1,5 +1,4 @@
 import Link from 'next/link';
-import React from 'react';
 
 import RegisterForm from '@/components/auth/register-form';
 import { APP_ROUTES } from '@/constants/app-routes';
@@ -9,40 +8,38 @@ export default async function Register() {
   await requireGuest();
 
   return (
-    <React.Fragment>
-      <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <img
-            alt="Your Company"
-            src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
-            className="mx-auto h-10 w-auto dark:hidden"
-          />
-          <img
-            alt="Your Company"
-            src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
-            className="mx-auto h-10 w-auto not-dark:hidden"
-          />
+    <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <img
+          alt="Your Company"
+          src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
+          className="mx-auto h-10 w-auto dark:hidden"
+        />
+        <img
+          alt="Your Company"
+          src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
+          className="mx-auto h-10 w-auto not-dark:hidden"
+        />
 
-          <h2 className="text-foreground mt-10 text-center text-2xl font-bold tracking-tight">
-            Create your account
-          </h2>
-        </div>
-
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          {/* form server action */}
-          <RegisterForm />
-
-          <p className="mt-10 text-center text-sm/6 text-gray-500 dark:text-gray-400">
-            {'Already have an account? '}
-            <Link
-              href={APP_ROUTES.AUTH.LOGIN}
-              className="font-semibold text-indigo-600 hover:text-indigo-500 dark:text-indigo-400 dark:hover:text-indigo-300"
-            >
-              Sign in
-            </Link>
-          </p>
-        </div>
+        <h2 className="text-foreground mt-10 text-center text-2xl font-bold tracking-tight">
+          Create your account
+        </h2>
       </div>
-    </React.Fragment>
+
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        {/* form server action */}
+        <RegisterForm />
+
+        <p className="mt-10 text-center text-sm/6 text-gray-500 dark:text-gray-400">
+          {'Already have an account? '}
+          <Link
+            href={APP_ROUTES.AUTH.LOGIN}
+            className="font-semibold text-indigo-600 hover:text-indigo-500 dark:text-indigo-400 dark:hover:text-indigo-300"
+          >
+            Sign in
+          </Link>
+        </p>
+      </div>
+    </div>
   );
 }
--- components/auth/login-form/index.tsx
+++ components/auth/login-form/index.tsx
@@ -31,9 +31,9 @@ export default function LoginForm() {
 
   return (
     <form
-      className="space-y-6"
       onSubmit={form.handleSubmit(onSubmit)}
       noValidate
+      className="space-y-6"
     >
       <InputWithLabel
         id="email"
@@ -52,9 +52,61 @@ export default function LoginForm() {
         {...form.register('password')}
       />
 
-      <Button type="submit" variant="primary" className="w-full">
-        Sign in
-      </Button>
+      {/* <div className="flex items-center justify-between">
+        <div className="flex gap-3">
+          <div className="flex h-6 shrink-0 items-center">
+            <div className="group grid size-4 grid-cols-1">
+              <input
+                id="remember-me"
+                name="remember-me"
+                type="checkbox"
+                className="col-start-1 row-start-1 appearance-none rounded-sm border border-gray-300 bg-white checked:border-indigo-600 checked:bg-indigo-600 indeterminate:border-indigo-600 indeterminate:bg-indigo-600 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:border-gray-300 disabled:bg-gray-100 disabled:checked:bg-gray-100 dark:border-white/10 dark:bg-white/5 dark:checked:border-indigo-500 dark:checked:bg-indigo-500 dark:indeterminate:border-indigo-500 dark:indeterminate:bg-indigo-500 dark:focus-visible:outline-indigo-500 forced-colors:appearance-auto"
+              />
+              <svg
+                fill="none"
+                viewBox="0 0 14 14"
+                className="pointer-events-none col-start-1 row-start-1 size-3.5 self-center justify-self-center stroke-white group-has-disabled:stroke-gray-950/25"
+              >
+                <path
+                  d="M3 8L6 11L11 3.5"
+                  strokeWidth={2}
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="opacity-0 group-has-checked:opacity-100"
+                />
+                <path
+                  d="M3 7H11"
+                  strokeWidth={2}
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="opacity-0 group-has-indeterminate:opacity-100"
+                />
+              </svg>
+            </div>
+          </div>
+          <label
+            htmlFor="remember-me"
+            className="block text-sm/6 text-gray-900 dark:text-gray-300"
+          >
+            Remember me
+          </label>
+        </div>
+
+        <div className="text-sm/6">
+          <a
+            href="#"
+            className="font-semibold text-indigo-600 hover:text-indigo-500 dark:text-indigo-400 dark:hover:text-indigo-300"
+          >
+            Forgot password?
+          </a>
+        </div>
+      </div> */}
+
+      <div>
+        <Button type="submit" variant="primary" className="w-full">
+          Sign in
+        </Button>
+      </div>
     </form>
   );
 }
