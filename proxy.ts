13822b4881d3f0ca3c9baa6a846cc44ecd7c50f7
vctorgriggi <victorgriggi21@gmail.com>
2025-12-12 17:39:30 -0400

    Rename middleware.ts to proxy.ts, update function name

--- proxy.ts
+++ proxy.ts
@@ -2,7 +2,7 @@ import { type NextRequest } from 'next/server';
 
 import { updateSession } from '@/lib/supabase/proxy';
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   // update user's auth session
   return await updateSession(request);
 }
